b5e7a7e5dd85f7e75fcfa8292a50c7af9f6ecc79
Dayo <dayoadewuyi@protonmail.com>
2022-09-30 17:54:47 +0100

    quick-fix

--- src/components/Staking.jsx
+++ src/components/Staking.jsx
@@ -5,16 +5,18 @@ import { useMoralis } from "react-moralis"
 import { stakingContractAddress, nftContractAddress } from '../constants/stakingContractAddress'
 import nftAbi from '../constants/nftAbi.json'
 import stakingAbi from '../constants/stakingAbi.json'
-import { Alchemy } from "alchemy-sdk";
+import { Network, Alchemy } from "alchemy-sdk";
 import '../assets/css/staking.css'
 
 
 
 const Staking = () => {
-  const alchemy = new Alchemy();
+  const settings = {
+    apiKey: "0AhEi7ZgISHTLrreg8I-AxOfdmhLzRhC", // Replace with your Alchemy API Key.
+    network: Network.ETH_MAINNET, // Replace with your network.
+  };
+  const alchemy = new Alchemy(settings);
   const { account } = useMoralis();
-  // eslint-disable-next-line
-  const [approved, setApproved] = useState(false)
   const [balance, setBalance] = useState(0)
   // eslint-disable-next-line
   const [stakedTokens, setStakedTokens] = useState([])
@@ -27,19 +29,20 @@ const Staking = () => {
 
   //cart functionality
   const [cart, setCart] = useState([]);
-  // eslint-disable-next-line
   const pushToCart = (id) => {
     setCart((prev) => [...prev, id]);
   };
-
-  // eslint-disable-next-line
   const stakeAllinCart = async () => {
     if (cart.length < 1) return;
     const contract = stakingContract();
     const NFTContract = nftContract();
     try {
-      await NFTContract.setApprovalForAll(stakingContractAddress);
+      const bool = await contract.isApprovedForAll(account, stakingContractAddress);
+      if (!bool) {
+        await NFTContract.setApprovalForAll(stakingContractAddress, true);
+      }
       await contract.stake(cart);
+
     } catch (error) {
       console.log(error);
     }
@@ -84,7 +87,7 @@ const Staking = () => {
 
     try {
       const result = await contract.userStakeInfo(account);
-      console.log(result);
+   
       const [_tokenStaked, _availableRewards] = result;
       if (_tokenStaked) {
         const tokensStaked = _tokenStaked.toNumber();
@@ -120,7 +123,7 @@ const Staking = () => {
     try {
       for (let i = 0; i < userNfts; i++) {
         const result = await contract.stakerAddress(userNfts[i].tokenId)
-        if (result === account) {
+        if (result === "0x4d26817ee0d948fff7f262acdb7038cde41f717d") {
           cleanNft.push(userNfts[i].tokenId);
         }
       };
@@ -135,29 +138,17 @@ const Staking = () => {
 
     }
   }
-  const getApproval = async (tokenId) => {
-    const provider = new ethers.providers.Web3Provider(ethereum);
-    const signer = provider.getSigner();
-    const nftContract = new ethers.Contract(nftContractAddress, nftAbi.abi, signer);
-
-    try {
-
-      await nftContract.approve(stakingContractAddress, tokenId)
-
-      setApproved(true)
-
-    } catch (err) {
-      console.log(err)
-    }
-  }
-  // eslint-disable-next-line
   const stakeNft = async (tokenId) => {
     const contract = stakingContract()
+    const NFTContract = nftContract();
     let approvedToken = []
     approvedToken.push(tokenId)
     try {
+      const bool = await contract.isApprovedForAll(account, stakingContractAddress);
+      if (!bool) {
+        await NFTContract.setApprovalForAll(stakingContractAddress, true);
+      }
       await contract.stake(approvedToken);
-
     }
     catch (error) {
       console.log(error)
@@ -165,11 +156,12 @@ const Staking = () => {
     }
   }
 
-  // eslint-disable-next-line
-  const withdrawStake = async () => {
-    const contract = stakingContract()
+  const withdrawStake = async (id) => {
+    const contract = stakingContract();
+    let unstaked = [];
+    unstaked.push(id);
     try {
-      await contract.withdraw(stakedTokens);
+      await contract.withdraw(unstaked);
 
     }
     catch (error) {
@@ -225,10 +217,21 @@ const Staking = () => {
                     <h3>Available tokens for claim</h3>
                     {availableRewards}
                   </li >
-
                   <button className="staking--btn" onClick={() => getReward()} >
                     <h3>
                       Claim Rewards
+                    </h3>
+                  </button>
+
+                </ul>
+                <ul className="staking-list-2">
+                  <li className="staking-listitems">
+                    <h3>Tokens in Cart</h3>
+                    {cart.length}
+                  </li >
+                  <button className="staking--btn" onClick={() => stakeAllinCart()} >
+                    <h3>
+                      Stake all in cart
                     </h3>
                   </button>
                 </ul>
@@ -237,15 +240,14 @@ const Staking = () => {
             <div className="row">
               <div className="heading">
                 <h3>Staked and Unstaked NFTS</h3>
-                <p>To stake, you have to approve first then stake.</p>
               </div>
               {/* Div that displays all nfts */}
               <div className="flex-parent">
                 {userNfts?.map((nft) => <UserNFTCard
                   nft={nft}
                   key={nft.tokenId}
+                  pushToCart={pushToCart}
                   arrOfStakedTokens={arrOfStakedTokens}
-                  getApproval={getApproval}
                   withdrawStake={withdrawStake}
                   stakeNft={stakeNft}
                 />)}
@@ -269,7 +271,7 @@ export default Staking;
 
 
 
-const UserNFTCard = ({ nft, arrOfStakedTokens, getApproval, withdrawStake, stakeNft }) => {
+const UserNFTCard = ({ nft, arrOfStakedTokens, pushToCart, withdrawStake, stakeNft }) => {
   const { rawMetadata, media } = nft;
   let approved = false;
   if (arrOfStakedTokens.includes(nft.tokenId)) {
@@ -286,14 +288,15 @@ const UserNFTCard = ({ nft, arrOfStakedTokens, getApproval, withdrawStake, stake
         {!approved ?
           <>
             <div className="flex gap-2 px-4 justify-between w-full">
-              {/* Approve token */}
-              <button className="approve-btn" onClick={() => getApproval(nft.tokenId)} >Approve</button>
+              {/* Push tokenID to Cart */}
+              <button className="approve-btn" onClick={() => pushToCart(nft.tokenId)} >Add to Cart</button>
               {/* Call stake function here */}
               <button className="approve-btn" onClick={() => stakeNft(nft.tokenId)} >Stake</button>
             </div>
           </> :
           <div className="flex gap-2 px-4 justify-between w-full harry items-center">
             <span className="text-xl">Staked</span>
+            {/* Unstake token */}
             <button className="approve-btn" onClick={() => withdrawStake(nft.tokenId)} >Un-Stake</button>
           </div>
         }
