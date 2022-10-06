53cc63daccf1d4ca9c2e73014028407d9b9c88ae
Dayo <dayoadewuyi@protonmail.com>
2022-10-06 17:19:58 +0100

    tst

--- src/components/Staking.jsx
+++ src/components/Staking.jsx
@@ -6,47 +6,28 @@ import { stakingContractAddress, nftContractAddress } from '../constants/staking
 import nftAbi from '../constants/nftAbi.json'
 import stakingAbi from '../constants/stakingAbi.json'
 import { Network, Alchemy } from "alchemy-sdk";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import '../assets/css/staking.css'
 
 
 
 const Staking = () => {
+
+
+
   const settings = {
     apiKey: "0AhEi7ZgISHTLrreg8I-AxOfdmhLzRhC", // Replace with your Alchemy API Key.
     network: Network.ETH_MAINNET, // Replace with your network.
   };
   const alchemy = new Alchemy(settings);
-  const { account } = useMoralis();
+  const { isWeb3Enabled, account } = useMoralis();
   const [balance, setBalance] = useState(0)
-  // eslint-disable-next-line
-  const [stakedTokens, setStakedTokens] = useState([])
   const [availableRewards, setAvailableRewards] = useState()
   const [tokenStaked, setTokenStaked] = useState();
-  // eslint-disable-next-line
   const [arrOfStakedTokens, setArrOfStakedTokens] = useState([]);
   const [userNfts, setUserNfts] = useState([])
   const { ethereum } = window;
-
-  //cart functionality
-  const [cart, setCart] = useState([]);
-  const pushToCart = (id) => {
-    setCart((prev) => [...prev, id]);
-  };
-  const stakeAllinCart = async () => {
-    if (cart.length < 1) return;
-    const contract = stakingContract();
-    const NFTContract = nftContract();
-    try {
-      const bool = await contract.isApprovedForAll(account, stakingContractAddress);
-      if (!bool) {
-        await NFTContract.setApprovalForAll(stakingContractAddress, true);
-      }
-      await contract.stake(cart);
-
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
   const stakingContract = () => {
     const provider = new ethers.providers.Web3Provider(ethereum);
@@ -62,18 +43,51 @@ const Staking = () => {
 
     return nftContract;
   }
+  /**Global declaration of staking contract and nft contract */
+  const STKContract = stakingContract();
+  const NFTContract = nftContract();
 
-  const userBalance = async () => {
+  //cart functionality
+  const [cart, setCart] = useState([]);
+  //push to cart function
+  const pushToCart = (tokenId) => {
+    setCart([...cart, tokenId])
+  }
+  //stake all in cart function
+  const stakeAllinCart = async () => {
+    if (cart.length < 1) {
+      toast.error('Cart is Empty, Stake an NFT', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      return;
+    }
+    try {
+      await STKContract.stake(cart);
 
-    const provider = new ethers.providers.Web3Provider(ethereum);
-    const signer = provider.getSigner();
-    const nftContract = new ethers.Contract(nftContractAddress, nftAbi.abi, signer);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  //set Approval for all NFTs
+  const setApprovalForAll = async () => {
+    try {
+      await NFTContract.setApprovalForAll(stakingContractAddress, true);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  //Wrapper function for get user NFTs function & get staked tokens and available rewards function
+  const getUsersNFTandRewardsInfo = async () => {
 
     try {
-
-      const result = await nftContract.balanceOf(account);
-      const bal = result.toNumber();
-      setBalance(bal)
       getAllUserNft();
       userStakedNftsInfo();
     } catch (err) {
@@ -82,12 +96,11 @@ const Staking = () => {
 
   }
 
+  //get all user staked NFTs and available rewards
   const userStakedNftsInfo = async () => {
-    const contract = stakingContract()
 
     try {
-      const result = await contract.userStakeInfo(account);
-   
+      const result = await STKContract.userStakeInfo(account);
       const [_tokenStaked, _availableRewards] = result;
       if (_tokenStaked) {
         const tokensStaked = _tokenStaked.toNumber();
@@ -109,25 +122,40 @@ const Staking = () => {
     }
 
   }
+  // Get all user NFTs, filter out the Legendary Owl NFTs and push them to the userNfts state
   const getAllUserNft = async () => {
     const nfts = (await alchemy.nft.getNftsForOwner(account)).ownedNfts
     const userNft = nfts.filter(obj => obj.contract.address === '0x6928d682911c068fbbcaeb1a409b9ab34fabfb91')
     setUserNfts(userNft);
-    console.log(userNft);
+    setBalance(userNft.length)
+    
   }
 
+  // Asynchronous functions inside useEffect to fetch user's NFTs
+  useEffect(async () => {
+    if (account) {
+      getUsersNFTandRewardsInfo();
+      getUserStakedNft();
+    }
+
+  }, [account])
+
+
+
+
+  //check all usernfts tokenID in the stakerAddress mapping to check if they're staked
+  //if they are, push them to the arrOfStakedTokens state
   const getUserStakedNft = async () => {
-    const contract = stakingContract()
     const cleanNft = [];
 
     try {
       for (let i = 0; i < userNfts; i++) {
-        const result = await contract.stakerAddress(userNfts[i].tokenId)
-        if (result === "0x4d26817ee0d948fff7f262acdb7038cde41f717d") {
+        const result = await STKContract.stakerAddress(userNfts[i].tokenId)
+        if (result === account) {
           cleanNft.push(userNfts[i].tokenId);
         }
       };
-      console.log(cleanNft);
+
       setArrOfStakedTokens(cleanNft);
 
 
@@ -135,63 +163,103 @@ const Staking = () => {
     catch (error) {
       console.log(error);
       setArrOfStakedTokens([]);
-
     }
   }
+  /**Function to stake a single nft, push token id into an array, check if approved, then stake */
   const stakeNft = async (tokenId) => {
-    const contract = stakingContract()
-    const NFTContract = nftContract();
     let approvedToken = []
-    approvedToken.push(tokenId)
+    approvedToken.push(tokenId);
     try {
-      const bool = await contract.isApprovedForAll(account, stakingContractAddress);
-      if (!bool) {
-        await NFTContract.setApprovalForAll(stakingContractAddress, true);
-      }
-      await contract.stake(approvedToken);
+      await STKContract.stake(approvedToken);
+
+      toast.success('NFT staked successfully!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
     }
     catch (error) {
       console.log(error)
 
+      toast.error('Failed to stake token', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
     }
   }
-
+  //function to unstake a single nft, push token id into an array, then unstake
   const withdrawStake = async (id) => {
-    const contract = stakingContract();
     let unstaked = [];
     unstaked.push(id);
     try {
-      await contract.withdraw(unstaked);
-
+      await STKContract.withdraw(unstaked);
+      toast.success('NFT unstaked successfully!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
     }
     catch (error) {
-      console.log(error)
+      console.log(error);
 
+      toast.error('Request Failed', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
     }
 
   }
-
+  //function to claim rewards
   const getReward = async () => {
-    const contract = stakingContract()
 
     try {
-      await contract.claimRewards();
-
+      await STKContract.claimRewards();
+      toast.success('Rewards Claimed successfully!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      })
     }
     catch (error) {
       console.log(error)
 
+      toast.error('Request Failed!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
     }
 
   }
 
 
 
-  useEffect(() => {
-    userBalance()
-    getUserStakedNft();
-
-  }, [account])
 
   return (
     <section className="faq sections" id="staking">
@@ -234,6 +302,11 @@ const Staking = () => {
                       Stake all in cart
                     </h3>
                   </button>
+                  <button className="staking--btn" onClick={() => setApprovalForAll()} >
+                    <h3>
+                      Approve NFTS for Staking
+                    </h3>
+                  </button>
                 </ul>
               </nav>
             </div>
@@ -261,6 +334,7 @@ const Staking = () => {
               <h3 className="error-h3"> OOPS!! You Need A Legendary Owl To Stake.</h3>
             </div>
           </div>}
+        <ToastContainer />
       </div>
     </section>
   );
@@ -282,7 +356,7 @@ const UserNFTCard = ({ nft, arrOfStakedTokens, pushToCart, withdrawStake, stakeN
     <div className="flex-child">
       <div className="staking-card">
         <div className="staking-card-image">
-          <img loading="lazy" decoding="async" src={media[0]?.thumbnail} alt={rawMetadata?.name} />
+          <img loading="lazy" decoding="async" src={media[0]?.gateway} alt={rawMetadata?.name} />
         </div>
         <h3 className="nft-name">Legendary Owl {rawMetadata?.name}</h3>
         {!approved ?
